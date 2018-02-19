041322383a429fbcf8219c14f728ef83b4b804c8
jemmarramizares <36613320+jemmarramizares@users.noreply.github.com>
2018-02-19 13:07:09 +0800

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,5 @@
+num=input("Input Number:")
+sum=num
+for i in range(1,num):
+    sum=sum*(num-i)
+print 'the factor of', num, 'is', sum 
---
+++ 3.py
@@ -0,0 +1,5 @@
+for i in range(1,10):
+    for v in range(0,i):
+        print i,
+    print '\n'
+input ()
